d431050b8b99526f673709e1512c6d2e88346d9a
Darmo117 <darmo117@gmail.com>
2022-10-22 12:28:14 +0200

    add escape unicode operation

--- api/operations/language/_unicode.py
+++ api/operations/language/_unicode.py
@@ -32,3 +32,51 @@ class RemoveDiacritics(_core.Operation):
 
     def apply(self, s: str) -> str:
         return unidecode.unidecode(s)
+
+
+class EscapeUnicodeChars(_core.Operation):
+    """Escape all or some Unicode characters."""
+
+    def __init__(self, mode: str = r'utf16be', encode_all_chars: bool = False, uppercase_hex: bool = False):
+        """Create a Unicode escape operation.
+
+        :param mode: The prefix to prepend to the codepoints.
+        :param encode_all_chars: Whether to escape all characters instead of only those with a codepoint > 127.
+        :param uppercase_hex: Whether to print hex codepoints as upper case.
+        """
+        self._functions = {
+            'utf16be': self._escape_to_utf16be,
+            'python': self._escape_to_python,
+        }
+        if mode not in self._functions:
+            raise ValueError(f'invalid mode: {mode}')
+        self._mode = mode
+        self._encode_all_chars = encode_all_chars
+        self._uppercase_hex = uppercase_hex
+
+    def get_params(self) -> typ.Dict[str, typ.Any]:
+        return {
+            'mode': self._mode,
+            'encode_all_chars': self._encode_all_chars,
+            'uppercase_hex': self._uppercase_hex,
+        }
+
+    def apply(self, s: str) -> str:
+        return ''.join(self._functions[self._mode](c)
+                       if ord(c) > 127 or self._encode_all_chars else c
+                       for c in s)
+
+    def _escape_to_python(self, c: str) -> str:
+        flag = 'X' if self._uppercase_hex else 'x'
+        codepoint = ord(c)
+        if codepoint > 0xffff:
+            return r'\U' + format(codepoint, '08' + flag)
+        else:
+            return r'\u' + format(codepoint, '04' + flag)
+
+    def _escape_to_utf16be(self, c: str) -> str:
+        b = bytes(c, 'utf-16be')
+        return ''.join(
+            r'\u' + format(int.from_bytes(b[i:i + 2], 'big'), '04' + ('X' if self._uppercase_hex else 'x'))
+            for i in range(0, len(b), 2)
+        )

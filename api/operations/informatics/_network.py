df569ac0376ac0dad5ffbd0ca150d1e73fd3d73b
Darmo117 <darmo117@gmail.com>
2022-10-27 00:12:23 +0200

    add group_ip_addresses operation

--- api/operations/informatics/_network.py
+++ api/operations/informatics/_network.py
@@ -1,6 +1,9 @@
+import ipaddress
 import re
+import typing as typ
 
 from .. import _core
+from ... import utils
 
 
 class DefangIpAddresses(_core.Operation):
@@ -33,3 +36,62 @@ fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]+| # fe80::7:8%eth0   fe80::7:8%1 (link
     def apply(self, s: str) -> str:
         s = self.IPV4_REGEX.sub(lambda m: m.group().replace('.', '[.]'), s)
         return self.IPV6_REGEX.sub(lambda m: m.group().replace(':', '[:]'), s)
+
+
+class GroupIpAddresses(_core.Operation):
+    """Group a list of IP addresses. Supports both IPv4 and IPv6."""
+
+    def __init__(self, subnet: int = 24, expand: bool = False, delimiter: str = '\n'):
+        """Create an IP address grouping operation.
+
+        :param subnet: The subnet mask.
+        :param expand: Whether to expand IP addresses.
+        :param delimiter: The string to use to split IP addresses.
+        """
+        if subnet < 0:
+            raise ValueError(f'subnet mask must be > 0, got {subnet}')
+        self._subnet = subnet
+        self._expand = expand
+        self._delimiter = utils.unescape(delimiter)
+
+    def get_params(self) -> typ.Dict[str, typ.Any]:
+        return {
+            'subnet': self._subnet,
+            'expand': self._expand,
+            'delimiter': self._delimiter,
+        }
+
+    def apply(self, s: str) -> str:
+        ipv4_networks = {}
+        ipv6_networks = {}
+
+        for line in s.split(self._delimiter):
+            try:
+                ip = ipaddress.ip_address(line)
+                mask = min(self._subnet, 128 if isinstance(ip, ipaddress.IPv6Address) else 32)
+                network = ipaddress.ip_interface(f'{line}/{mask}').network
+            except ValueError:
+                continue
+            if isinstance(network, ipaddress.IPv4Network):
+                if network not in ipv4_networks:
+                    ipv4_networks[network] = []
+                ipv4_networks[network].append(ip)
+            else:
+                if network not in ipv6_networks:
+                    ipv6_networks[network] = []
+                ipv6_networks[network].append(ip)
+
+        return f'{self._format(ipv4_networks)}{self._format(ipv6_networks)}'.strip()
+
+    @staticmethod
+    def _format(networks: typ.Dict[ipaddress.IPv4Network | ipaddress.IPv6Network,
+                                   typ.List[ipaddress.IPv4Address | ipaddress.IPv6Address]]) -> str:
+        res = ''
+        for network in sorted(networks.keys()):
+            ips = networks[network]
+            ips.sort()
+            res += network.compressed + '\n'
+            for ip in ips:
+                res += f'  {ip.compressed}\n'
+            res += '\n'
+        return res

07b8f697f81b656c305420409c5e66092c063eb2
Darmo117 <darmo117@gmail.com>
2022-10-29 20:19:56 +0200

    add operations for lists of numbers

--- api/operations/math/__init__.py
+++ api/operations/math/__init__.py
@@ -1,3 +1,4 @@
 from ._geometry import *
+from ._number_lists import *
 from ._sets import *
 from ._units import *
---
+++ api/operations/math/_number_lists.py
@@ -0,0 +1,82 @@
+import abc
+import functools
+import operator
+import typing as typ
+
+import numpy as np
+
+from .. import _core
+
+_Number = int | float
+
+
+class _Base(_core.Operation, abc.ABC):
+    """Base class for number list operations."""
+
+    def __init__(self, sep: str = ','):
+        """Create an operation for number lists.
+
+        :param sep: String to use to split numbers.
+        """
+        self._sep = sep
+
+    def get_params(self) -> typ.Dict[str, typ.Any]:
+        return {
+            'sep': self._sep,
+        }
+
+    def apply(self, s: str) -> str:
+        return str(self._op(map(float, s.split(self._sep))))
+
+    @abc.abstractmethod
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        pass
+
+
+class Sum(_Base):
+    """Compute the sum of a list of numbers."""
+
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        return sum(numbers)
+
+
+class Subtract(_Base):
+    """Compute the difference of a list of numbers."""
+
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        return functools.reduce(operator.sub, numbers)
+
+
+class Multiply(_Base):
+    """Compute the product of a list of numbers."""
+
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        return functools.reduce(operator.mul, numbers)
+
+
+class Divide(_Base):
+    """Compute the quotient of a list of numbers."""
+
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        return functools.reduce(operator.truediv, numbers)
+
+
+class Mean(_Base):
+    """Compute the mean of a list of numbers."""
+
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        return float(np.mean(numbers))
+
+
+class Median(_Base):
+    """Compute the median of a list of numbers."""
+
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        return float(np.median(numbers))
+
+
+class Stdev(_Base):
+    """Compute the standard deviation of a list of numbers."""
+
+    def _op(self, numbers: typ.Iterable[_Number]) -> _Number:
+        return float(np.std(numbers))
